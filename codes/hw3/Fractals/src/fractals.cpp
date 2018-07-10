432050f4079324f5cb3e0f57e408736d08a27061
Samuel Peng <samuelpeng1@gmail.com>
2018-07-10 11:53:37 -0700

    adding hw3 starter folders

---
+++ codes/hw3/Fractals/src/fractals.cpp
@@ -0,0 +1,14 @@
+// This is the CPP file you will edit and turn in. (TODO: Remove this comment!)
+
+#include "fractals.h"
+using namespace std;
+
+void drawSierpinskiTriangle(GWindow& window, double x, double y, double size, int order) {
+    // TODO: write this function
+
+}
+
+void drawTree(GWindow& gw, double x, double y, double size, int order) {
+    // TODO: write this function
+
+}
---
+++ codes/hw3/GrammarSolver/src/grammarsolver.cpp
@@ -0,0 +1,11 @@
+// This is the CPP file you will edit and turn in. (TODO: Remove this comment!)
+
+#include "grammarsolver.h"
+using namespace std;
+
+Vector<string> grammarGenerate(istream& input, string symbol, int times) {
+    // TODO: write this function
+    
+    Vector<string> v;   // this is only here so it will compile
+    return v;           // this is only here so it will compile
+}
